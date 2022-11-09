804b350824dd4b95db5ee5947a78c5e7e8558f65
leatg <202140783@edu.clg.qc.ca>
2022-11-09 16:50:38 -0500

    update

---
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.net.Uri;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ImageView imgDebut = (ImageView) findViewById(R.id.imgDebut);
+
+
+    }
+}
